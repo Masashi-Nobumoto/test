49caacce19399f7e159a33e27599a7f100d99836
(Masashi Nobumoto) <(nobmanobma@gmail.com)>
2022-09-06 05:52:13 +0900

    1

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
-st.checkbox("チェックボックス") #引数に入れることでboolを返す
-st.button("ボタン") #引数に入れるとboolで返す
-st.selectbox("メニューリスト", ("選択肢1", "選択肢2", "選択肢3")) #第一引数：リスト名、第二引数：選択肢
-st.multiselect("メニューリスト（複数選択可）", ("選択肢1", "選択肢2", "選択肢3")) #第一引数：リスト名、第二引数：選択肢、複数選択可
-st.radio("ラジオボタン", ("選択肢1", "選択肢2", "選択肢3")) #第一引数：リスト名（選択肢群の上に表示）、第二引数：選択肢
-# 以下をサイドバーに表示
-st.sidebar.text_input("文字入力欄") #引数に入力内容を渡せる
-st.sidebar.text_area("テキストエリア")
+dataframe = pd.DataFrame(np.random.randn(10, 20),
+  columns = ('col %d' % i
+    for i in range(20)))
+dataframe
+
+dataframe = pd.DataFrame(np.random.randn(10, 5),
+  columns = ('col %d' % i
+    for i in range(5)))
+dataframe
+st.write('This is a line_chart.')
+st.line_chart(dataframe)
